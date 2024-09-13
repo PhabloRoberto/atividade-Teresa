082aea1d543efa09f3df03f3e2ce1d069f335e7f
PhabloRoberto <122638815+PhabloRoberto@users.noreply.github.com>
2024-09-13 20:04:31 -0300

    inicio de um sonho

---
+++ site-pessoal/src/pages/Home/index.jsx
@@ -0,0 +1,36 @@
+import './style.css'
+
+function Home() {
+  
+
+  return (
+    <div>
+    <header> {/*cabeça do site */}
+        <h2>Phablo Roberto</h2>
+        <nav>
+          <a href="#">link</a>
+          <a href="#">link</a>
+          <a href="#">link</a>
+          <a href="#">link</a>
+        </nav>
+    </header>
+    <main>
+      <div className='conteiner'>
+      <img className='imagem-inicial'src="././public/inicial.jpg" alt="imagem-inicial" />
+       
+
+       <h1>Phablo</h1>
+       
+      </div>
+
+      
+    </main>
+    <footer>
+
+    </footer>
+    
+    </div>
+  )
+}
+
+export default Home
