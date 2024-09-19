8c4d2f4a4ae6c9cfa18ae5543b2f4ca07435f04b
PhabloRoberto <122638815+PhabloRoberto@users.noreply.github.com>
2024-09-19 20:47:30 -0300

    ok

--- site-pessoal/src/pages/Home/home.js
+++ site-pessoal/src/pages/Home/home.js
@@ -1,27 +1,6 @@
-import './style.css'
-
-function Home() {
-  return (
-    <div>
-      
-      {/*Cabeça do site */}
-      <header>
-        <div className='perfil-header'>
-          <h2>Phablo Roberto</h2>
-          <img src="/public/perfilTest.jpeg" alt="" className='mini-foto'/>
-        </div>
-        {/*Links do site*/}
-        <nav>
-          <a href="#Home">Home</a>
-          <a href="#sobre_mim">Sobre mim</a>
-          <a href="#projetos">Projetos</a>
-          <a href="#Contatos">Contato</a>
-        </nav>
-      </header>
-      
-      {/*Copo do site */}
-
-      <main>
+export default function Home() {
+    return (
+        <main>
         {/*Home do site*/}
         <div className="home">
           {/*imagem da pagina inicial */}
@@ -70,8 +49,6 @@ function Home() {
 
         </div>
       </main>
-    </div>
-  )
+    )
 }
 
-export default Home;
