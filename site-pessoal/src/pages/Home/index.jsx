3fde347895a3158f646c73355cd300156898a0b8
PhabloRoberto <122638815+PhabloRoberto@users.noreply.github.com>
2024-09-19 18:55:13 -0300

    estamos quase lá
    só é preciso ser feito os cards e as 5 curiosidades e os projetos e footer e resposividade

--- site-pessoal/src/pages/Home/index.jsx
+++ site-pessoal/src/pages/Home/index.jsx
@@ -3,38 +3,69 @@ import './style.css'
 function Home() {
   return (
     <div>
+      
+      {/*Cabeça do site */}
       <header>
         <div className='perfil-header'>
           <h2>Phablo Roberto</h2>
           <img src="/public/perfilTest.jpeg" alt="" className='mini-foto'/>
         </div>
+        {/*Links do site*/}
         <nav>
-          <a href="#">Home</a>
-          <a href="#">Sobre mim</a>
-          <a href="#sobre_mim">Projetos</a>
-          <a href="#">Contato</a>
+          <a href="#Home">Home</a>
+          <a href="#sobre_mim">Sobre mim</a>
+          <a href="#projetos">Projetos</a>
+          <a href="#Contatos">Contato</a>
         </nav>
       </header>
+      
+      {/*Copo do site */}
 
       <main>
+        {/*Home do site*/}
         <div className="home">
-          <img src="./public/perfilTest.jpeg" alt="" />
-          <div className="frase">
-            <h1>Phablo Roberto</h1>
-            <p>Cada dia melhorando</p>
-          </div>
-        </div>
-
-        <div className="sobre_mim" id="sobre_mim">
-          <h1>Sobre mim</h1>
-          <div className="container1">
-            <div className="mini-texto">
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sequi laborum, totam quos perspiciatis quaerat accusantium iste in tempora incidunt aspernatur rerum labore autem ex error sed ipsa rem laudantium?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id temporibus, eaque ab minima magni sit alias quo quis optio, sed expedita quam vel ipsa nemo non odio odit unde cum.</p>
+          {/*imagem da pagina inicial */}
+          <img src="./public/foto-fundo.jpg" alt="" className='img-main' />
+            <div className="frase">
+              <h1>Phablo Roberto</h1>
+              <p>Cada dia melhorando</p>
             </div>
-            <img src="/public/perfilTest.jpeg" alt="" />
+        </div>
+        {/*Falando sobre mim */}
+        <div className="sobre_mim" id="sobre_mim">
+          <div className="container-sobre">
+            <h1>Sobre mim</h1>
+          </div>
+          <div className="texto_sobre">
+            <div className='caixa'>
+              <div className='foto_minha'>
+                <img src="./public/perfilTest.jpeg" alt=""/>
+              </div>
+              <div className='texto-me'>
+                <h2>Phablo Roberto</h2>
+                <p>Olá meu nome é Phablo e tenho 18 anos de idade, sou estudante so IFRN (Instituto Federal do Rio Grande do Norte) faço o curso de Infomática na qual não me arrependo em ter feito.</p>
+              </div>
+            </div>
           </div>
         </div>
 
+        <div id='Curiosidade'>
+            <div className="cards">
+
+            </div>
+            <div className="cards">
+              
+            </div>
+            <div className="cards">
+              
+            </div>
+            <div className="cards">
+              
+            </div>
+            <div className="cards">
+              
+            </div>
+        </div>
         <div>
 
         </div>
