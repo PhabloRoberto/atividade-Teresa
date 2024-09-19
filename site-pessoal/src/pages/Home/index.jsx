9e8be2a653f2e54013c22eddaa209232f66efa28
PhabloRoberto <122638815+PhabloRoberto@users.noreply.github.com>
2024-09-19 11:10:41 -0300

    site teresa

--- site-pessoal/src/pages/Home/index.jsx
+++ site-pessoal/src/pages/Home/index.jsx
@@ -1,36 +1,46 @@
 import './style.css'
 
 function Home() {
-  
-
   return (
     <div>
-    <header> {/*cabeça do site */}
-        <h2>Phablo Roberto</h2>
+      <header>
+        <div className='perfil-header'>
+          <h2>Phablo Roberto</h2>
+          <img src="/public/perfilTest.jpeg" alt="" className='mini-foto'/>
+        </div>
         <nav>
-          <a href="#">link</a>
-          <a href="#">link</a>
-          <a href="#">link</a>
-          <a href="#">link</a>
+          <a href="#">Home</a>
+          <a href="#">Sobre mim</a>
+          <a href="#sobre_mim">Projetos</a>
+          <a href="#">Contato</a>
         </nav>
-    </header>
-    <main>
-      <div className='conteiner'>
-      <img className='imagem-inicial'src="././public/inicial.jpg" alt="imagem-inicial" />
-       
+      </header>
 
-       <h1>Phablo</h1>
-       
-      </div>
+      <main>
+        <div className="home">
+          <img src="./public/perfilTest.jpeg" alt="" />
+          <div className="frase">
+            <h1>Phablo Roberto</h1>
+            <p>Cada dia melhorando</p>
+          </div>
+        </div>
 
-      
-    </main>
-    <footer>
+        <div className="sobre_mim" id="sobre_mim">
+          <h1>Sobre mim</h1>
+          <div className="container1">
+            <div className="mini-texto">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sequi laborum, totam quos perspiciatis quaerat accusantium iste in tempora incidunt aspernatur rerum labore autem ex error sed ipsa rem laudantium?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id temporibus, eaque ab minima magni sit alias quo quis optio, sed expedita quam vel ipsa nemo non odio odit unde cum.</p>
+            </div>
+            <img src="/public/perfilTest.jpeg" alt="" />
+          </div>
+        </div>
 
-    </footer>
-    
+        <div>
+
+        </div>
+      </main>
     </div>
   )
 }
 
-export default Home
+export default Home;
